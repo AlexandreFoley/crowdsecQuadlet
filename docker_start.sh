949b4af961b5e4a6c5e0af39adbecc2fccd930ae
Alexandre Foley <Alexandre.Foley@outlook.com>
2025-08-07 18:11:22 -0400

    error in the script and container.

--- docker_start.sh
+++ docker_start.sh
@@ -279,14 +279,14 @@ conf_set_if "$LOCAL_API_URL" '.url = strenv(LOCAL_API_URL)' "$lapi_credentials_p
 if istrue "$DISABLE_LOCAL_API"; then
     # we only use the envvars that are actually defined
     # in case of persistent configuration
-    if [  "$AGENT_USERNAME" != "" ] then;
+    if [  "$AGENT_USERNAME" != "" ]; then
        conf_set_if "$AGENT_USERNAME" '.login = strenv(AGENT_USERNAME)' "$lapi_credentials_path" 
     fi
-    if [  "$AGENT_PASSWORD" != "" ] then;
+    if [  "$AGENT_PASSWORD" != "" ]; then
         conf_set_if "$AGENT_PASSWORD" '.password = strenv(AGENT_PASSWORD)' "$lapi_credentials_path"
     fi
-    if [  "$AGENT_AUTO_REGISTRATION_TOKEN" != "" ] then;
-        if [  "$AGENT_USERNAME" != "" ] then;
+    if [  "$AGENT_AUTO_REGISTRATION_TOKEN" != "" ]; then
+        if [  "$AGENT_USERNAME" != "" ]; then
            cscli lapi register --url "$LOCAL_API_URL" --token "$AGENT_AUTO_REGISTRATION_TOKEN" --machine "$AGENT_USERNAME"
         else
             cscli lapi register --url "$LOCAL_API_URL" --token "$AGENT_AUTO_REGISTRATION_TOKEN"
